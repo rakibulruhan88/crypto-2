0546e48a5d06d05f9ef0dd3039f973cb938d5262
rakibulruhan88 <mdrohan7224@gmail.com>
2024-08-25 17:26:51 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ menu.addEventListener("click", () => {
   nav.classList.toggle("active");
   menu.classList.toggle("bx-x");
 });
+
+let tabs = document.querySelectorAll(".tab");
+let cards = document.querySelectorAll(".cards");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove("click");
+    });
+    cards.forEach((tab) => {
+      tab.classList.remove("click-card");
+    });
+    tab.classList.add("click");
+    cards[index].classList.add("click-card");
+  });
+});
